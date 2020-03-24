6fda9dd1b2731e7d1258c48244c4e5ad76609da6
SpyingEnvelope <gadi800@gmail.com>
2020-03-23 20:39:42 -0600

    finished all the tests

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,4 +1,13 @@
-const leapYears = function() {
+const leapYears = function(a) {
+    let b;
+
+    if (a % 4 == 0 && a % 100 != 0 || a % 400 == 0) {
+        b = true;
+        return b;
+    } else {
+        b = false;
+        return b;
+    }
 
 }
 
--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,27 @@
-const ftoc = function() {
-
+const ftoc = function(a) {
+  let conversionResult;
+  conversionResult = (a - 32) * (5/9);
+  
+  if (conversionResult != 0 && conversionResult % 1 != 0) {
+    conversionResult = conversionResult.toFixed(1);
+    conversionResult = conversionResult * 1;
+    return conversionResult;
+  } else {
+    return conversionResult;
+  }
 }
 
-const ctof = function() {
+const ctof = function(a) {
+  let conversionResult;
+  conversionResult = (a * 9/5) + 32;
 
+  if (conversionResult != 0 && conversionResult % 1 != 0) {
+    conversionResult = conversionResult.toFixed(1);
+    conversionResult = conversionResult * 1;
+    return conversionResult;
+  } else {
+    return conversionResult;
+  }
 }
 
 module.exports = {
