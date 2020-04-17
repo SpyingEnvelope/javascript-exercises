34e1b39a268c6621dd405932c00cca7af901b9d1
SpyingEnvelope <gadi800@gmail.com>
2020-04-17 09:46:32 -0600

    finished getTheTitles exercise

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,4 +1,9 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    for(let title in books){
+        let myArray = [books[0].title];
+        myArray.push(books[1].title);
+        return myArray;
+    }
 
 }
 
