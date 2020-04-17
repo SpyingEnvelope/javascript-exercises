02d603243b2e2955cf1db70fe77c362ea60d5f68
SpyingEnvelope <gadi800@gmail.com>
2020-04-17 08:28:20 -0600

    finished fibnoacci exercise

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+    let myArray = [0, 1, 1];
 
+    a = a * 1;
+    
+    if(a < 0){
+        return 'OOPS';
+    }
+
+    for(let i = 3; i <= a; i++){
+        myArray.push(myArray[i - 2] + myArray[i - 1]);
+    }
+
+    a = myArray[a];
+
+    return a;
 }
 
 module.exports = fibonacci
