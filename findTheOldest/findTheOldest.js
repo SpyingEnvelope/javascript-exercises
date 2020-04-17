dc9bc901fdc1faaa2a2d8bbefcab68874f497c30
SpyingEnvelope <gadi800@gmail.com>
2020-04-17 11:13:56 -0600

    basic code written for findTheOldest

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,38 @@
-let findTheOldest = function() {
+const people = [
+    {
+      name: 'Carly',
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: 'Ray',
+      yearOfBirth: 1962,
+      yearOfDeath: 2011
+    },
+    {
+      name: 'Jane',
+      yearOfBirth: 1912,
+      yearOfDeath: 1941
+    },
+  ]
 
+let findTheOldest = function(people) {
+    
+    let tester = [0, 0, 0];
+
+    for(let i = 0; i < 3; i++){
+        tester[i] = people[i];
+    };
+    
+    let testerSort = tester.sort(function(a, b){
+        if((a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth)){
+            return false;
+        } else {
+            return true;
+        }
+    })
+
+    return tester;
 }
 
 module.exports = findTheOldest
