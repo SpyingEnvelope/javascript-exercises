70df6d5f9d7bcedad34799d3368cfe514ec9ca7b
Windows SpyingEnvy <gadi800@gmail.com>
2020-04-17 23:21:19 -0600

    finished first test of findTheOldest

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,38 +1,20 @@
-const people = [
-    {
-      name: 'Carly',
-      yearOfBirth: 1942,
-      yearOfDeath: 1970,
-    },
-    {
-      name: 'Ray',
-      yearOfBirth: 1962,
-      yearOfDeath: 2011
-    },
-    {
-      name: 'Jane',
-      yearOfBirth: 1912,
-      yearOfDeath: 1941
-    },
-  ]
-
 let findTheOldest = function(people) {
     
-    let tester = [0, 0, 0];
+  let tester = [0, 0, 0];
 
-    for(let i = 0; i < 3; i++){
-        tester[i] = people[i];
-    };
-    
-    let testerSort = tester.sort(function(a, b){
-        if((a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth)){
-            return false;
-        } else {
-            return true;
-        }
-    })
+  for(let i = 0; i < 3; i++){
+      tester[i] = people[i];
+  };
+  
+  let testerSort = tester.sort(function(a, b){
+      if((a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth)){
+          return -1;
+      } else {
+          return 1;
+      }
+  })
 
-    return tester;
+  return tester[0];
 }
 
 module.exports = findTheOldest
